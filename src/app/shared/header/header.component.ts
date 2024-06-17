ece15f48a102ce96b502427fdd578ac83892003a
CodeRacon <michael.buschmann.86@gmail.com>
2024-06-17 21:38:36 +0200

    feat: #1 generated header & footer component; basic styling

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [BurgerMenuComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
