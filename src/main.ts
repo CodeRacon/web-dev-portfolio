7dd5a16159cfce1181398d5d812b133a648eb30b
CodeRacon <michael.buschmann.86@gmail.com>
2024-06-18 13:33:45 +0200

    feat: #6 implemented 'Tornis' library for body background animation; wrote logic to watch mouse-position, styled dot-cover-up

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,33 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { watchViewport, TornisUpdateValues } from 'tornis';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+/**
+ * gets mouse-position-data to be used for the subtile mouse-over animation on the body elements background (dots).
+ * This is achieved by updating CSS variables with the current mouse position and a radius.
+ * This function is called by the `watchViewport` function from the `Tornis` library
+ * whenever the mouse position changes.
+ *
+ * @param mouse - An object containing the current mouse position and whether it has changed.
+ */
+const applyMouseEffect = ({ mouse }: TornisUpdateValues) => {
+  if (mouse.changed) {
+    const mouseX = mouse.x;
+    const mouseY = mouse.y;
+    const radius = 8 * 16;
+
+    const bodyElement = document.querySelector('body');
+    if (bodyElement) {
+      bodyElement.style.setProperty('--mouse-x', `${mouseX}px`);
+      bodyElement.style.setProperty('--mouse-y', `${mouseY}px`);
+      bodyElement.style.setProperty('--radius', `${radius}px`);
+    }
+  }
+};
+
+watchViewport(applyMouseEffect);
