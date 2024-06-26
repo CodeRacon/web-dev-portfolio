5dd46e8e048a68b37a24fb8b9497504cb04fca4c
CodeRacon <michael.buschmann.86@gmail.com>
2024-06-26 22:16:01 +0200

    feat: #7 implemented shadow-directives

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TextShadowOffsetDirective } from '../../shared/common/text-shadow-offset.directive';
+import { BoxShadowOffsetDirective } from '../../shared/common/box-shadow-offset.directive';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [TextShadowOffsetDirective, BoxShadowOffsetDirective],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
