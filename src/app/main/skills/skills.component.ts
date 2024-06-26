c69bd1c95a59423b2e34dd8ab31d076c6af5e7c2
CodeRacon <michael.buschmann.86@gmail.com>
2024-06-26 22:17:41 +0200

    feat: #10 implemented shadow-directives

--- src/app/main/skills/skills.component.ts
+++ src/app/main/skills/skills.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
+import { TextShadowOffsetDirective } from '../../shared/common/text-shadow-offset.directive';
+import { BoxShadowOffsetDirective } from '../../shared/common/box-shadow-offset.directive';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [TextShadowOffsetDirective, BoxShadowOffsetDirective],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
   techStack = [
     { caption: 'Angular', icon: 'angular' },
+    { caption: 'TypeScript', icon: 'ts' },
+    { caption: 'JavaScript', icon: 'js' },
     { caption: 'API', icon: 'api' },
-    { caption: 'CSS', icon: 'css' },
     { caption: 'Firebase', icon: 'firebase' },
     { caption: 'Git', icon: 'git' },
-    { caption: 'HTML', icon: 'html' },
-    { caption: 'JavaScript', icon: 'js' },
-    { caption: 'TypeScript', icon: 'ts' },
-    { caption: 'Scrum', icon: 'scrum' },
     { caption: 'Material Design', icon: 'md' },
+    { caption: 'CSS', icon: 'css' },
+    { caption: 'Scrum', icon: 'scrum' },
+    { caption: 'HTML', icon: 'html' },
   ];
 }
