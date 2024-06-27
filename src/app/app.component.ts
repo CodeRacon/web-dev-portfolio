730c6068ef3b6d4477d9d955cda33d7e22396ce5
CodeRacon <michael.buschmann.86@gmail.com>
2024-06-28 00:00:11 +0200

    feat: #14 generated index-arrow.component; implemented it into app.component; styled + edited content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { HeroComponent } from './main/hero/hero.component';
 import { AboutComponent } from './main/about/about.component';
 import { SkillsComponent } from './main/skills/skills.component';
 import { PortfolioComponent } from './main/portfolio/portfolio.component';
+import { IndexArrowComponent } from './shared/index-arrow/index-arrow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    IndexArrowComponent,
     HeaderComponent,
     HeroComponent,
     AboutComponent,
