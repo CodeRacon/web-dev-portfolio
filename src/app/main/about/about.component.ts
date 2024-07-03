f0f3c3ffa75d86fe0ec3676dc1deab2498375799
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-03 16:18:57 +0200

    feat: #7 updated html structure to be able to render text in chosen laguage dynamically; implemented TranslateModule in component.ts

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { TextShadowOffsetDirective } from '../../shared/common/text-shadow-offset.directive';
 import { BoxShadowOffsetDirective } from '../../shared/common/box-shadow-offset.directive';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [TextShadowOffsetDirective, BoxShadowOffsetDirective],
+  imports: [
+    TextShadowOffsetDirective,
+    BoxShadowOffsetDirective,
+    TranslateModule,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
