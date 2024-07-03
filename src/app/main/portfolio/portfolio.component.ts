9879d3f9baf40799e383b8646451e9eebeaa7390
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-03 16:21:11 +0200

    feat: #11 updated html structure to be able to render text in chosen laguage dynamically; implemented TranslateModule & logic to get textdata for description from language json

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TextShadowOffsetDirective } from '../../shared/common/text-shadow-offset.directive';
 import { BoxShadowOffsetDirective } from '../../shared/common/box-shadow-offset.directive';
+import { TranslateModule } from '@ngx-translate/core';
 
 export interface Project {
   name: string;
-  description: string;
+  descriptionKey: string;
   technologies: string[];
   video: string;
   links: ProjectLink[];
@@ -18,7 +19,11 @@ export interface ProjectLink {
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [TextShadowOffsetDirective, BoxShadowOffsetDirective],
+  imports: [
+    TextShadowOffsetDirective,
+    BoxShadowOffsetDirective,
+    TranslateModule,
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
@@ -26,43 +31,31 @@ export class PortfolioComponent {
   projectList: Project[] = [
     {
       name: 'Join',
+      descriptionKey: 'portfolio.projects.join.description',
       technologies: ['JavaScript | Firebase | HTML | CSS'],
-      description:
-        'Kanban-based project management tool designed to enhance workflow & collaboration within your team.',
       video: 'join.mp4',
       links: [
         { name: 'GitHub', url: 'https://github.com/CodeRacon/join' },
-        {
-          name: 'Live',
-          url: 'https://www.join.michael-buschmann.dev/',
-        },
+        { name: 'Live', url: 'https://www.join.michael-buschmann.dev/' },
       ],
     },
     {
       name: 'CavernQuest',
+      descriptionKey: 'portfolio.projects.cavernQuest.description',
       technologies: ['JavaScript | OOP | Firebase | HTML | CSS'],
-      description:
-        "2D jump'n run browser game, where you help Woozle conquering a mossy cavern full of mystery and danger.",
       video: 'cq.mp4',
       links: [
-        {
-          name: 'GitHub',
-          url: 'https://github.com/CodeRacon/CavernQuest',
-        },
+        { name: 'GitHub', url: 'https://github.com/CodeRacon/CavernQuest' },
         { name: 'Live', url: 'https://www.cq.michael-buschmann.dev/' },
       ],
     },
     {
       name: 'PokeDex',
+      descriptionKey: 'portfolio.projects.pokeDex.description',
       technologies: ['REST API | JavaScript | HTML |  CSS'],
-      description:
-        'PokeAPI-powered Pokemon-Database to get all the information you need about your favorite little monsters!',
       video: 'poke.mp4',
       links: [
-        {
-          name: 'GitHub',
-          url: 'https://github.com/CodeRacon/PokeDex',
-        },
+        { name: 'GitHub', url: 'https://github.com/CodeRacon/PokeDex' },
         { name: 'Live', url: 'https://www.pokedex.michael-buschmann.dev/' },
       ],
     },
