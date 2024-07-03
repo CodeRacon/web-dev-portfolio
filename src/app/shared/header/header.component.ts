1b2d9b0317f6f6e84d7774259342b21ba638aece
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-03 16:15:14 +0200

    feat: #15 create central control-element to toggle language;  header; logic to remember chosen language after reload (localStorage)

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
 import { BurgerIconComponent } from './burger-icon/burger-icon.component';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [BurgerIconComponent, BurgerMenuComponent],
+  imports: [BurgerIconComponent, BurgerMenuComponent, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  currentLang: string;
+
+  constructor(private translate: TranslateService) {
+    const savedLang = localStorage.getItem('preferredLanguage');
+    this.currentLang = savedLang || 'en';
+    this.translate.use(this.currentLang);
+  }
+
+  changeLanguage(lang: string): void {
+    this.translate.use(lang);
+    this.currentLang = lang;
+    localStorage.setItem('preferredLanguage', lang);
+  }
+
   scrollToSection(sectionId: string, offset = 0): void {
     const element = document.getElementById(sectionId);
     if (element) {
