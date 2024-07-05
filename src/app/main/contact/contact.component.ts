2c36d493cc96a74d68c2f4724a6d18962ff507c1
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-05 12:35:41 +0200

    feat: #13 created, implemented & edited contact.component; implemented Angulars ReactiveFormsModule for validation purpose

--- src/app/main/contact/contact.component.ts
+++ src/app/main/contact/contact.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  standalone: true,
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss'],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+  @ViewChild('message') messageTextarea!: ElementRef;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      policy: [false, Validators.requiredTrue],
+    });
+  }
+
+  ngAfterViewInit() {
+    this.checkOverflow();
+    this.contactForm.get('message')?.valueChanges.subscribe(() => {
+      this.checkOverflow();
+    });
+  }
+
+  checkOverflow() {
+    const textarea = this.messageTextarea.nativeElement;
+    const legend = textarea.closest('fieldset').querySelector('legend');
+
+    if (textarea.scrollHeight > textarea.clientHeight) {
+      legend.classList.add('has-overflow');
+    } else {
+      legend.classList.remove('has-overflow');
+    }
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    }
+  }
 }
