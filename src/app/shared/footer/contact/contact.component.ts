25b2631fdd40a741e348e496d33c17dd7c7c0634
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-08 17:59:16 +0200

    feat: #13 logic for dynamic form-validation, using the Angular built in tools; logic to check for overflow on textarea (special styling)

---
+++ src/app/shared/footer/contact/contact.component.ts
@@ -0,0 +1,63 @@
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {
+  nameValidator,
+  emailValidator,
+  messageValidator,
+} from './custom-validators';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss'],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+})
+export class ContactComponent {
+  contactForm: FormGroup;
+  @ViewChild('message') messageTextarea!: ElementRef;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, nameValidator()]],
+      email: ['', [Validators.required, emailValidator()]],
+      message: ['', [Validators.required, messageValidator()]],
+      policy: [false, Validators.requiredTrue],
+    });
+  }
+
+  get isFormValid(): boolean {
+    return this.contactForm.valid;
+  }
+
+  ngAfterViewInit() {
+    this.checkOverflow();
+    this.contactForm.get('message')?.valueChanges.subscribe(() => {
+      this.checkOverflow();
+    });
+  }
+
+  checkOverflow() {
+    const textarea = this.messageTextarea.nativeElement;
+    const legend = textarea.closest('fieldset').querySelector('legend');
+
+    if (textarea.scrollHeight > textarea.clientHeight) {
+      legend.classList.add('has-overflow');
+    } else {
+      legend.classList.remove('has-overflow');
+    }
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+    }
+  }
+}
---
+++ src/app/shared/footer/contact/custom-validators.ts
@@ -0,0 +1,25 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function nameValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const nameRegex =
+      /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/;
+    const valid = nameRegex.test(control.value);
+    return valid ? null : { invalidName: true };
+  };
+}
+
+export function emailValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const valid = emailRegex.test(control.value);
+    return valid ? null : { invalidEmail: true };
+  };
+}
+
+export function messageValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    return isWhitespace ? { whitespace: true } : null;
+  };
+}
