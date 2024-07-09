b6a8c144fbe5d62b8b969416f8f40659b6683a54
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-10 01:01:34 +0200

    feat: #2 implemented logic to jump to main-content sections via nav-menu when on routerOutlet

--- src/app/shared/header/burger-menu/burger-menu.component.ts
+++ src/app/shared/header/burger-menu/burger-menu.component.ts
@@ -2,16 +2,19 @@ import { Component, Input, AfterViewInit } from '@angular/core';
 import { TextShadowOffsetDirective } from '../../common/text-shadow-offset.directive';
 import { BoxShadowOffsetDirective } from '../../common/box-shadow-offset.directive';
 import { BurgerIconComponent } from '../burger-icon/burger-icon.component';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [TextShadowOffsetDirective, BoxShadowOffsetDirective],
+  imports: [TextShadowOffsetDirective, BoxShadowOffsetDirective, RouterModule],
   selector: 'app-burger-menu',
   templateUrl: './burger-menu.component.html',
   styleUrls: ['./burger-menu.component.scss'],
 })
 export class BurgerMenuComponent implements AfterViewInit {
   @Input() burgerIcon!: BurgerIconComponent;
+
+  constructor(private router: Router) {}
 
   ngAfterViewInit() {
     const burgerMenu = document.getElementById('burger-menu');
@@ -23,6 +26,14 @@ export class BurgerMenuComponent implements AfterViewInit {
         }
       });
     }
+  }
+
+  async navigateAndScroll(sectionId: string, offset = 0): Promise<void> {
+    if (this.router.url !== '/') {
+      await this.router.navigate(['/']);
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+    this.scrollToSection(sectionId, offset);
   }
 
   scrollToSection(sectionId: string, offset = 0): void {
