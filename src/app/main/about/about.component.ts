82a76cd18c4e4cfb7cdc25d0cbf37abbe38676cb
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-12 14:45:36 +0200

    doc: added jsdoc

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TextShadowOffsetDirective } from '../../shared/common/text-shadow-offset.directive';
 import { BoxShadowOffsetDirective } from '../../shared/common/box-shadow-offset.directive';
 import { TranslateModule } from '@ngx-translate/core';
+import { ScrollAnimationDirective } from '../../shared/common/scroll-animation.directive';
 
 @Component({
   selector: 'app-about',
@@ -10,11 +11,18 @@ import { TranslateModule } from '@ngx-translate/core';
     TextShadowOffsetDirective,
     BoxShadowOffsetDirective,
     TranslateModule,
+    ScrollAnimationDirective,
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
 export class AboutComponent {
+  /**
+   * Scrolls the page to the specified section with an optional offset.
+   *
+   * @param sectionId - The ID of the section to scroll to.
+   * @param offset - The optional offset in pixels to apply to the scroll position.
+   */
   scrollToSection(sectionId: string, offset = 0): void {
     const element = document.getElementById(sectionId);
     if (element) {
