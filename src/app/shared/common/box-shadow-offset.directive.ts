3df262af4779e6f4a047a7c6673ee2121bd51d5d
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-12 15:02:21 +0200

    doc: #14 added jsdoc

--- src/app/shared/common/box-shadow-offset.directive.ts
+++ src/app/shared/common/box-shadow-offset.directive.ts
@@ -10,11 +10,19 @@ export class BoxShadowOffsetDirective implements OnInit, OnDestroy {
   @Input() maxOffset: number = 8;
   private subscription!: Subscription;
 
+  /**
+   * Constructs a new instance of the `BoxShadowOffsetDirective`.
+   * @param el - The `ElementRef` instance for the element the directive is applied to.
+   * @param shadowOffsetService - The `ShadowOffsetService` instance used to calculate the shadow offset.
+   */
   constructor(
     private el: ElementRef,
     private shadowOffsetService: ShadowOffsetService
   ) {}
 
+  /**
+   * Subscribes to the `shadowOffsetService.getMousePosition$()` observable and updates the box shadow of the element when the mouse position changes.
+   */
   ngOnInit() {
     this.subscription = this.shadowOffsetService
       .getMousePosition$()
@@ -23,12 +31,21 @@ export class BoxShadowOffsetDirective implements OnInit, OnDestroy {
       });
   }
 
+  /**
+   * Unsubscribes from the `shadowOffsetService` subscription when the directive is destroyed.
+   */
   ngOnDestroy() {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
 
+  /**
+   * Updates the box shadow of the element based on the current mouse position.
+   * The box shadow is offset by a maximum of `maxOffset` pixels in both the x and y directions.
+   * The box shadow color is set to `#f0dbc7`.
+   * The box shadow transition is set to a 0.125s ease-in-out transition for the box shadow, and a 0.675s ease-out transition for all other properties.
+   */
   private updateShadow() {
     const rect = this.el.nativeElement.getBoundingClientRect();
     const { x, y } = this.shadowOffsetService.calculateShadowOffset(
