a8c92063c822f9aa2ded3658a67c4298d3b24c40
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-12 15:17:01 +0200

    doc: #2 added jsdoc

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -19,18 +19,34 @@ import { RouterModule } from '@angular/router';
 export class HeaderComponent {
   currentLang: string;
 
+  /**
+   * Initializes the header component with the user's preferred language.
+   * Retrieves the preferred language from local storage, or defaults to 'en' if not found.
+   * Sets the current language and updates the translation service to use the preferred language.
+   */
   constructor(private translate: TranslateService) {
     const savedLang = localStorage.getItem('preferredLanguage');
     this.currentLang = savedLang || 'en';
     this.translate.use(this.currentLang);
   }
 
+  /**
+   * Changes the current language and updates the translation service and local storage.
+   *
+   * @param lang - The new language to set.
+   */
   changeLanguage(lang: string): void {
     this.translate.use(lang);
     this.currentLang = lang;
     localStorage.setItem('preferredLanguage', lang);
   }
 
+  /**
+   * Scrolls the page to the specified section with an optional offset.
+   *
+   * @param sectionId - The ID of the section to scroll to.
+   * @param offset - The number of pixels to offset the scroll position (default is 0).
+   */
   scrollToSection(sectionId: string, offset = 0): void {
     const element = document.getElementById(sectionId);
     if (element) {
