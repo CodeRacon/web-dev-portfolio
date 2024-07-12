20fbfbf983c74dab3beee2ebb0d9950154a47f6e
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-12 15:01:10 +0200

    doc: #13 added jsdoc

--- src/app/shared/common/email.service.ts
+++ src/app/shared/common/email.service.ts
@@ -8,8 +8,18 @@ import { Observable } from 'rxjs';
 export class EmailService {
   private apiUrl = 'https://michael-buschmann.dev/sendMail.php';
 
+  /**
+   * Constructs a new instance of the EmailService.
+   * @param http - The HttpClient instance to use for making HTTP requests.
+   */
   constructor(private http: HttpClient) {}
 
+  /**
+   * Sends an email using the provided form data.
+   *
+   * @param formData - The form data to be sent in the email.
+   * @returns An Observable that emits the response from the email server.
+   */
   sendEmail(formData: any): Observable<any> {
     return this.http.post(this.apiUrl, formData);
   }
