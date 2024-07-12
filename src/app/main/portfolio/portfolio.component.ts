b4828f3bb535be7aaedabf3c15f40b90a92d4139
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-12 14:54:17 +0200

    doc: #11 added jsdoc

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { TextShadowOffsetDirective } from '../../shared/common/text-shadow-offset.directive';
 import { BoxShadowOffsetDirective } from '../../shared/common/box-shadow-offset.directive';
 import { TranslateModule } from '@ngx-translate/core';
+import { ScrollAnimationDirective } from '../../shared/common/scroll-animation.directive';
 
+/**
+ * Represents a project in the portfolio, containing a name, description key, list of technologies used,
+ * a video file, and a list of links (name and URL).
+ */
 export interface Project {
   name: string;
   descriptionKey: string;
@@ -11,6 +16,9 @@ export interface Project {
   links: ProjectLink[];
 }
 
+/**
+ * Represents a link associated with a project, containing a name and URL.
+ */
 export interface ProjectLink {
   name: string;
   url: string;
@@ -23,11 +31,17 @@ export interface ProjectLink {
     TextShadowOffsetDirective,
     BoxShadowOffsetDirective,
     TranslateModule,
+    ScrollAnimationDirective,
   ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
+  /**
+   * An array of project objects, each representing a project in the portfolio.
+   * Each project has a name, a description key, a list of technologies used,
+   * a video file, and a list of links (name and URL).
+   */
   projectList: Project[] = [
     {
       name: 'Join',
@@ -61,10 +75,39 @@ export class PortfolioComponent {
     },
   ];
 
+  /**
+   * Handles the visibility of a video element on the page.
+   * If the element becomes visible, it will play the video after a 100ms delay.
+   * If the element becomes hidden, it will pause the video and reset the current time to 0.
+   *
+   * @param isVisible - A boolean indicating whether the element is visible or not.
+   * @param video - The HTMLVideoElement to play or pause.
+   */
+  onElementVisible(isVisible: boolean, video: HTMLVideoElement): void {
+    if (isVisible) {
+      setTimeout(() => {
+        this.playVideo(video);
+      }, 100);
+    } else {
+      this.pauseVideo(video);
+    }
+  }
+
+  /**
+   * Plays the provided video element.
+   *
+   * @param video - The HTMLVideoElement to play.
+   */
   playVideo(video: HTMLVideoElement): void {
+    video.muted = true;
     video.play();
   }
 
+  /**
+   * Pauses the provided video element and resets its current time to 0.
+   *
+   * @param video - The HTMLVideoElement to pause.
+   */
   pauseVideo(video: HTMLVideoElement): void {
     video.pause();
     video.currentTime = 0;
