80bcd21bab87fa866314a2c3f14df49fbf0d5118
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-12 15:07:03 +0200

    doc: added jsdoc

--- src/app/shared/common/scroll-observer.service.ts
+++ src/app/shared/common/scroll-observer.service.ts
@@ -5,6 +5,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ScrollObserverService {
+  /**
+   * Creates an Observable that emits an array of `IntersectionObserverEntry` objects
+   * whenever the observed element intersects with the root element or viewport.
+   *
+   * @param options - Optional configuration options for the `IntersectionObserver`.
+   * @returns An Observable that emits an array of `IntersectionObserverEntry` objects.
+   */
   createObserver(
     options?: IntersectionObserverInit
   ): Observable<IntersectionObserverEntry[]> {
@@ -21,6 +28,13 @@ export class ScrollObserverService {
     });
   }
 
+  /**
+   * Creates an Observable that emits the first `IntersectionObserverEntry` whenever the observed element intersects with the root element or viewport.
+   *
+   * @param element - The element to observe for intersection.
+   * @param options - Optional configuration options for the `IntersectionObserver`.
+   * @returns An Observable that emits the first `IntersectionObserverEntry` when the observed element intersects.
+   */
   observe(
     element: Element,
     options?: IntersectionObserverInit
