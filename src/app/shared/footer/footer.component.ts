f01b01e948b52ab2be1237c44b9311d535bb2501
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-12 15:09:36 +0200

    feat: #4 implemented TransformModule; added jsdoc

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ContactComponent, RouterModule],
+  imports: [ContactComponent, RouterModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  /**
+   * Scrolls the page to the specified section with an optional offset.
+   *
+   * @param sectionId - The ID of the section to scroll to.
+   * @param offset - The number of pixels to offset the scroll position (default is 0).
+   */
   scrollToSection(sectionId: string, offset = 0): void {
     const element = document.getElementById(sectionId);
     if (element) {
