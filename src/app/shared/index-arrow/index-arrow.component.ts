efc815f5367f4f41043d9159902c884a8092f7d9
CodeRacon <michael.buschmann.86@gmail.com>
2024-07-12 15:23:01 +0200

    feat / doc: #14 removed Listener for mousedOver/-Out, animated only per scroll-animation; added jsdoc

--- src/app/shared/index-arrow/index-arrow.component.ts
+++ src/app/shared/index-arrow/index-arrow.component.ts
@@ -20,34 +20,58 @@ export class IndexArrowComponent {
   @Input() sectionId: string = this.generateUniqueId();
   @Input() maxOffset: number = 4;
 
+  /**
+   * Represents the current state of the animation for the index arrow component.
+   * The animation state can be one of the following:
+   * - 'idle': The arrow is in its initial state, not animating.
+   * - 'animatingForward': The arrow is animating in the forward direction.
+   * - 'expanded': The arrow is in its expanded state after the forward animation.
+   * - 'animatingBackward': The arrow is animating in the backward direction.
+   */
   private animationState:
     | 'idle'
     | 'animatingForward'
     | 'expanded'
     | 'animatingBackward' = 'idle';
 
+  /**
+   * Constructs an instance of the `IndexArrowComponent`.
+   *
+   * @param elementRef - A reference to the DOM element of the component.
+   * @param renderer - A service for rendering DOM elements.
+   * @param shadowOffsetService - A service for managing the shadow offset of the component.
+   */
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2,
     private shadowOffsetService: ShadowOffsetService
   ) {}
 
-  @HostListener('mouseover')
-  onMouseOver(): void {
-    this.triggerAnimation('forward');
-  }
-
-  @HostListener('mouseleave')
-  onMouseLeave(): void {
-    this.triggerAnimation('backward');
-  }
-
   @HostListener('window:mousemove', ['$event'])
+
+  /**
+   * Updates the shadow effect of the index arrow component based on the mouse event.
+   *
+   * This method is called when the window's `mousemove` event is triggered. It calculates the shadow offset based on the bounding rectangle of the component and the maximum offset value, and then updates the `dx` and `dy` attributes of the `feDropShadow` element in the SVG to apply the calculated shadow offset.
+   *
+   * @param event - The `MouseEvent` object containing the mouse movement information.
+   */
   onMouseMove(event: MouseEvent): void {
     this.updateShadow(event);
   }
 
-  private triggerAnimation(direction: 'forward' | 'backward'): void {
+  /**
+   * Triggers the animation of the index arrow component based on the provided direction.
+   *
+   * - If the direction is 'forward' and the animation state is not 'idle', the function returns without triggering any animation.
+   * - If the direction is 'backward' and the animation state is not 'expanded', the function returns without triggering any animation.
+   * - If the direction is 'forward', the function triggers the forward animation by finding the corresponding SVGAnimateElement and calling its `beginElement()` method. The animation state is then set to 'animatingForward'.
+   * - If the direction is 'backward', the function triggers the backward animation by finding the corresponding SVGAnimateElement and calling its `beginElement()` method. The animation state is then set to 'animatingBackward'.
+   * - After the animation is triggered, the function sets a timeout to update the animation state to 'expanded' or 'idle' after 500 milliseconds.
+   *
+   * @param direction - The direction of the animation, either 'forward' or 'backward'.
+   */
+  triggerAnimation(direction: 'forward' | 'backward'): void {
     if (direction === 'forward' && this.animationState !== 'idle') return;
     if (direction === 'backward' && this.animationState !== 'expanded') return;
 
@@ -68,10 +92,25 @@ export class IndexArrowComponent {
     }
   }
 
+  /**
+   * Generates a unique identifier string for a section.
+   *
+   * The identifier is generated using a random number and a substring of its base 36 representation.
+   *
+   * @returns A unique identifier string in the format `section_<random_string>`.
+   */
   private generateUniqueId(): string {
     return `section_${Math.random().toString(36).substr(2, 9)}`;
   }
 
+  /**
+   * Updates the shadow effect of the index arrow component based on the mouse event.
+   *
+   * This function calculates the shadow offset based on the bounding rectangle of the component and the maximum offset value.
+   * It then updates the `dx` and `dy` attributes of the `feDropShadow` element in the SVG to apply the calculated shadow offset.
+   *
+   * @param event - The mouse event that triggered the shadow update.
+   */
   private updateShadow(event: MouseEvent): void {
     const rect = this.elementRef.nativeElement.getBoundingClientRect();
     const { x, y } = this.shadowOffsetService.calculateShadowOffset(
